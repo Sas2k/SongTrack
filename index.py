14a741d8d2df73c3bf10ae885a34458ca37e6611
Sasen Perera <sasen.learnings@gmail.com>
2022-12-28 17:21:41 +0530

    v1.3: Updated .vercelignore and made the images and mp3 to call from github

--- index.py
+++ index.py
@@ -41,20 +41,22 @@ def index():
     songs = get_songs()
     song = get_random_song_info()
     # print(song)
-    return render_template('index.html', TrackNo=song[0], Artist=song[1], Title=song[2], Picture='/media/'+song[2].replace(" ", "_")+".png", File='https://github.com/Sas2k/SongTrack/raw/main/static/media/'+songs[int(song[0])-1].replace("[", "%5B").replace("]", "%5D")+".mp3")
+    return render_template('index.html', TrackNo=song[0], Artist=song[1], Title=song[2], Picture='https://github.com/Sas2k/SongTrack/raw/main/static/media/'+song[2].replace(" ", "_")+".png", File='https://github.com/Sas2k/SongTrack/raw/main/static/media/'+songs[int(song[0])-1].replace("[", "%5B").replace("]", "%5D")+".mp3")
 
 #For Production
 #File='https://github.com/Sas2k/SongTrack/raw/main/static/media/'+songs[int(song[0])-1].replace("[", "%5B").replace("]", "%5D")+".mp3"
+#Picture='https://github.com/Sas2k/SongTrack/raw/main/static/media/'+song[2].replace(" ", "_")+".png"
 
 #For Local Testing
 #File='/media/'+songs[int(song[0])-1]+".mp3"
+#Picture='/media/'+song[2].replace(" ", "_")+".png"
 
 @app.route('/songs/<int:track_no>/')
 def track(track_no):
     songs = get_songs()
     song = get_track_song(track_no-1)
     # print(song)
-    return render_template('song.html', TrackNo=song[0], Artist=song[1], Title=song[2], Picture='/media/'+song[2].replace(" ", "_")+".png", File='https://github.com/Sas2k/SongTrack/raw/main/static/media/'+songs[int(song[0])-1].replace("[", "%5B").replace("]", "%5D")+".mp3")
+    return render_template('song.html', TrackNo=song[0], Artist=song[1], Title=song[2], Picture='https://github.com/Sas2k/SongTrack/raw/main/static/media/'+song[2].replace(" ", "_")+".png", File='https://github.com/Sas2k/SongTrack/raw/main/static/media/'+songs[int(song[0])-1].replace("[", "%5B").replace("]", "%5D")+".mp3")
 
 @app.errorhandler(404)
 def page_not_found(e):
