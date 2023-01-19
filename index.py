2430c9bf655666d32493fb95c9a062820a16a01c
Sasen Perera <sasen.learnings@gmail.com>
2023-01-19 13:18:20 +0530

    7.0: Added Merry-go-round of life from Howl's moving castle and moved the url_for() to the index.py

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from random import randint
 
 app = Flask(__name__, static_folder='static')
@@ -22,7 +22,8 @@ def get_songs() -> list:
         "14-The_Legend_of_Zelda_Ocarina_of_Time[Koji_Kondo]-Gerudo_Valley",
         "15-The_Legend_of_Zelda_Breath_of_the_Wild_[Manaka_Kataoka,_Yasuaki_Iwata_and_Hajime_Wakai]-Main_Theme",
         "16-Super_Mario_3D_World_[Koji_Kondo]-Title_Theme",
-        "17-Kirby_and_The_forgotten_Land_[Yuta_Ogasawara_and_Hirokazu_Ando]-Title_Screen_Theme"
+        "17-Kirby_and_The_forgotten_Land_[Yuta_Ogasawara_and_Hirokazu_Ando]-Title_Screen_Theme",
+        "18-Howl's_Moving_Castle_[Joe_Hisaishi]-Merry-Go-Round_of_Life",
     ]
 
     songs.sort(key=lambda x: int(x.split('-')[0]))
@@ -30,16 +31,16 @@ def get_songs() -> list:
 
 def get_random_song_info() -> list:
     songs = get_songs()
-    song = str(songs[randint(0, len(songs) - 1)]).replace("_", " ").split('-')
+    song = str(songs[randint(0, len(songs) - 1)]).replace("_", " ").split('-', 2)
     return song
 
 def get_track_song(track_no) -> list:
     songs = get_songs()
     try:
-        song = str(songs[track_no]).replace("_", " ").split('-')
+        song = str(songs[track_no]).replace("_", " ").split('-', 2)
         return song
     except:
-        song = str(songs[0]).replace("_", " ").split('-')
+        song = str(songs[0]).replace("_", " ").split('-', 2)
         return song
 
 
@@ -48,20 +49,20 @@ def index():
     songs = get_songs()
     song = get_random_song_info()
     # print(song)
-    return render_template('index.html', TrackNo=song[0], Artist=song[1], Title=song[2], Tracks=len(songs), File='https://raw.githubusercontent.com/Sas2k/SongTrack/main/static/media/'+songs[int(song[0])-1].replace("(", "%28").replace(")", "%29").replace("[", "%5B").replace("]", "%5D")+".mp3", Picture='https://raw.githubusercontent.com/Sas2k/SongTrack/main/static/media/'+str(song[0])+'-'+song[2].replace(" ", "_").replace("(", "%28").replace(")", "%29").replace("[", "%5B").replace("]", "%5D")+".png")
+    return render_template('index.html', TrackNo=song[0], Artist=song[1], Title=song[2], Tracks=len(songs), File=url_for('static', filename='/media/'+songs[int(song[0])-1]+".mp3"), Picture=url_for('static' , filename='/media/'+str(song[0])+'-'+song[2].replace(" ", "_")+".png"))
 
 #For Production
 #File='https://raw.githubusercontent.com/Sas2k/SongTrack/main/static/media/'+songs[int(song[0])-1].replace("(", "%28").replace(")", "%29").replace("[", "%5B").replace("]", "%5D")+".mp3", Picture='https://raw.githubusercontent.com/Sas2k/SongTrack/main/static/media/'+str(song[0])+'-'+song[2].replace(" ", "_").replace("(", "%28").replace(")", "%29").replace("[", "%5B").replace("]", "%5D")+".png"
 
 #For Local Testing
-#File='/media/'+songs[int(song[0])-1]+".mp3", Picture='/media/'+str(song[0])+'-'+song[2].replace(" ", "_")+".png"
+#File=url_for('static', filename='/media/'+songs[int(song[0])-1]+".mp3"), Picture=url_for('static' , filename='/media/'+str(song[0])+'-'+song[2].replace(" ", "_")+".png")
 
 @app.route('/songs/<int:track_no>/')
 def track(track_no):
     songs = get_songs()
     song = get_track_song(track_no-1)
     # print(song)
-    return render_template('song.html', TrackNo=song[0], Artist=song[1], Title=song[2], Tracks=len(songs), File='https://raw.githubusercontent.com/Sas2k/SongTrack/main/static/media/'+songs[int(song[0])-1].replace("(", "%28").replace(")", "%29").replace("[", "%5B").replace("]", "%5D")+".mp3", Picture='https://raw.githubusercontent.com/Sas2k/SongTrack/main/static/media/'+str(song[0])+'-'+song[2].replace(" ", "_").replace("(", "%28").replace(")", "%29").replace("[", "%5B").replace("]", "%5D")+".png")
+    return render_template('song.html', TrackNo=song[0], Artist=song[1], Title=song[2], Tracks=len(songs), File=url_for('static', filename='/media/'+songs[int(song[0])-1]+".mp3"), Picture=url_for('static' , filename='/media/'+str(song[0])+'-'+song[2].replace(" ", "_")+".png"))
 
 @app.errorhandler(404)
 def page_not_found(e):
